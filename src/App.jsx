c6b8fc7b89eb3403eb213634ad6127b108348e24
SamaadTurner <samaad.turner@gmail.com>
2023-08-21 22:52:24 -0700

    niceee

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,71 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Form, Button, Card } from 'react-bootstrap';
+import './App.css';
+import axios from 'axios';
 
-function App() {
-  const [count, setCount] = useState(0)
+const api_key = import.meta.env.VITE_API_KEY;
+const apiURL = 'https://us1.locationiq.com/v1/search';
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      searchQuery: '',
+      apiResults: [],
+    };
+  }
+
+  setSearchQuery = (e) => {
+    this.setState({ searchQuery: e.target.value });
+  };
+
+  handleExploreClick = async (e) => {
+    e.preventDefault();
+    const { searchQuery } = this.state;
+    try {
+      const response = await axios.get(`${apiURL}?key=${api_key}&q=${searchQuery}&format=json`);
+      this.setState({ apiResults: response.data });
+      console.log(response.data);
+    } catch (error) {
+      console.log('Error fetching data', error);
+    }
+  };
+
+  render() {
+    const { apiResults } = this.state;
+
+    return (
+      <div style={{ display: 'block', width: 700, padding: 30 }}>
+        <h4>City Explorer</h4>
+        <Form onSubmit={this.handleExploreClick}>
+          <Form.Group>
+            <Form.Label>Enter city name:</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="seattle, tacoma, lakewood, etc."
+              onChange={this.setSearchQuery}
+            />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Explore!
+          </Button>
+        </Form>
+
+        {apiResults.map((location) => (
+          <Card key={location.place_id} className="mt-4">
+            <Card.Body>
+              <Card.Title>{location.display_name}</Card.Title>
+              <Card.Text>
+                <strong>Latitude:</strong> {location.lat}<br />
+                <strong>Longitude:</strong> {location.lon}
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    );
+  }
 }
 
-export default App
+export default App;
