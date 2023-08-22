2cb8cea97fbec7ef786a4200bbde183598ddb947
SamaadTurner <samaad.turner@gmail.com>
2023-08-22 13:17:48 -0700

    finshed lab6

--- README.md
+++ README.md
@@ -7,18 +7,16 @@ City-Explorer
 Imagine you want directions of a city of your picking. This assignment will demonstrate an understanding of API's and how to render requests to the screen.   
 
 ## Getting Started
-<!-- What are the steps that a user must take in order to build this app on their own machine and get it running? -->  
+You can just follow my link to my nelify and it is an already published site.
 
 ## Architecture  
-<!-- Provide a detailed description of the application design. What technologies (languages, libraries, etc) you're using, and any other relevant design information. -->  
-
+I used bootstrap to make cards. LocationIQ was used to fetch all information shown on screen.
 ## Change Log  
-<!-- Use this area to document the iterative changes made to your application as each feature is successfully implemented. Use time stamps. Here's an example:  
 
-01-01-2001 4:59pm - Application now has a fully-functional express server, with a GET route for the location resource. -->  
+08-22-2023 1:15pm - Application now has a fully-functional site that gathers information on a city that a user inquires about.
 
 ## Credit and Collaborations  
-<!-- Give credit (and a link) to other people or resources that helped you build this application. -->  
+Got help from a mixture of things including Jacob Knaack and ChatGPT
 
 
 
--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Form, Button, Card } from 'react-bootstrap';
 import './App.css';
 import axios from 'axios';
+import Map from './component/Map';
 
 const api_key = import.meta.env.VITE_API_KEY;
 const apiURL = 'https://us1.locationiq.com/v1/search';
@@ -13,6 +14,7 @@ class App extends React.Component {
     this.state = {
       searchQuery: '',
       apiResults: [],
+      error: null,
     };
   }
 
@@ -21,7 +23,9 @@ class App extends React.Component {
   };
 
   handleExploreClick = async (e) => {
+    console.log('Form Submitted');
     e.preventDefault();
+    this.setState({error: null});
     const { searchQuery } = this.state;
     try {
       const response = await axios.get(`${apiURL}?key=${api_key}&q=${searchQuery}&format=json`);
@@ -29,11 +33,16 @@ class App extends React.Component {
       console.log(response.data);
     } catch (error) {
       console.log('Error fetching data', error);
+      this.setState({ error: 'An error occurred while fetching data.' }); e
+
     }
   };
 
   render() {
-    const { apiResults } = this.state;
+    const { lat, lon, api_key } = this.props;
+    const staticMapUrl = `https://maps.locationiq.com/v3/staticmap?key=${api_key}&center=${lat},${lon}&zoom=9`;
+
+    const { apiResults, error } = this.state;
 
     return (
       <div style={{ display: 'block', width: 700, padding: 30 }}>
@@ -51,6 +60,7 @@ class App extends React.Component {
             Explore!
           </Button>
         </Form>
+        {error && <div style={{ color: 'red', fontSize: '16px', marginTop: '10px' }}>{error}</div>}
 
         {apiResults.map((location) => (
           <Card key={location.place_id} className="mt-4">
@@ -60,9 +70,11 @@ class App extends React.Component {
                 <strong>Latitude:</strong> {location.lat}<br />
                 <strong>Longitude:</strong> {location.lon}
               </Card.Text>
+              <Map location={location}/>
             </Card.Body>
           </Card>
         ))}
+        
       </div>
     );
   }
