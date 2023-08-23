45aef00654bc974bb67822459635e86b4c098ce1
SamaadTurner <samaad.turner@gmail.com>
2023-08-23 14:10:12 -0700

    finished up

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ class App extends React.Component {
     this.state = {
       searchQuery: '',
       apiResults: [],
+      forecasts: [],
       error: null,
     };
   }
@@ -28,9 +29,13 @@ class App extends React.Component {
     this.setState({error: null});
     const { searchQuery } = this.state;
     try {
-      const response = await axios.get(`${apiURL}?key=${api_key}&q=${searchQuery}&format=json`);
-      this.setState({ apiResults: response.data });
-      console.log(response.data);
+      const locationResponse = await axios.get(`${apiURL}?key=${api_key}&q=${searchQuery}&format=json`);
+      const expressServerResponse = await axios.get(`http://localhost:3001/weather?lat=${locationResponse.data[0].lat}&lon=${locationResponse.data[0].on}&searchQuery=${searchQuery}`);
+      this.setState({
+        apiResults: locationResponse.data,
+        forecasts: expressServerResponse.data
+      });
+      console.log(locationResponse.data, expressServerResponse.data);
     } catch (error) {
       console.log('Error fetching data', error);
       this.setState({ error: 'An error occurred while fetching data.' }); e
@@ -42,8 +47,8 @@ class App extends React.Component {
     const { lat, lon, api_key } = this.props;
     const staticMapUrl = `https://maps.locationiq.com/v3/staticmap?key=${api_key}&center=${lat},${lon}&zoom=9`;
 
-    const { apiResults, error } = this.state;
-
+    const { apiResults, error, forecasts } = this.state;
+    console.log(forecasts);
     return (
       <div style={{ display: 'block', width: 700, padding: 30 }}>
         <h4>City Explorer</h4>
@@ -74,7 +79,18 @@ class App extends React.Component {
             </Card.Body>
           </Card>
         ))}
-        
+
+        {forecasts.map((forecast, idx) => (
+          <Card key={idx} className="mt-4">
+            <Card.Body>
+              <Card.Title>Date: {forecast.date}</Card.Title>
+              <Card.Text>
+                <strong>Forecast: </strong> {forecast.description}<br />
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        ))}
+
       </div>
     );
   }
